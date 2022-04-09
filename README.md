6ca13c8c6b9cce8a8ce84fe557f0fb376ddd110a
gianlucasbandi <sbandi.gianluca@gmail.com>
2022-04-10 00:00:23 +0200

    modifiche a index e cartella articoli

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Titolo:
 
 Descrizione:
 	Pagina web che consente di consultare risorse al fine di organizzare la propria alimentazione; le risorse messe a disposizione sono di vario genere, quali articoli, ricette ed esperienze di altri. Inoltre, è possibile controllare le calorie e i macronutrienti di ogni singolo pasto, prima di aggiungerli alla propria dieta.
+
+<div style="display: grid; grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));">
--- js/functions.js
+++ js/functions.js
@@ -17,3 +17,35 @@ function childrenRow() {
 function result() {
     window.open("risultati.html")
 }
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+          slidesPerGroup: 2,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+          slidesPerGroup: 1,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+          slidesPerGroup: 1,
+        },
+      },
+  });
