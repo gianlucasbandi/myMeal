e251539963f9b80f358b9b51e5918bb7335218a8
alessiapontiggia <102748217+alessiapontiggia@users.noreply.github.com>
2022-05-23 00:27:06 +0200

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -64,3 +64,69 @@ let swiper = new Swiper(".mySwiper", {
       rowCount--;
     }
   }
+
+  
+  
+  //JQUERY
+
+  $(document).ready(function(){
+
+    // gallery fading images
+   
+      setInterval(function(){
+        $(".meal-img").fadeOut("slow", function(){
+          $(".meal-img").attr("src", "./img/second.jpg");
+          $(".meal-img").fadeIn("slow");
+          $(".meal-img").fadeOut("slow", function(){
+            $(".meal-img").attr("src", "./img/first.jpg");
+            $(".meal-img").fadeIn("slow");
+          });
+        });
+        /*
+        $(".meal-img").fadeOut("slow");
+        $(".meal-img").attr("src", "./img/first.jpg");
+        $(".meal-img").fadeIn("slow");
+        */
+      }, 3000);
+    
+   
+    // first-category
+    var original = $("#centered1").text();
+       
+        $("#centered1").mouseenter(function(){
+        $("#centered1").text("Vuoi conoscere le nostre letture consigliate sulla dieta e scoprire i consigli degli esperti del settore?").css("font-size", "18px");
+        $("#centered1").css("background-color", "rgba(255, 255, 255, 0.4)");
+      });
+      $("#centered1").mouseleave(function(){
+        $("#centered1").text(original).css("font-size","28px");
+        $("#centered1").css("font-style", "normal");
+        $("#centered1").css("background-color", "transparent");
+      });
+    
+    // second category
+      var or2 = $("#centered2").text();
+      $("#centered2").mouseenter(function(){
+        $("#centered2").text("Scopri le nostre letture consigliate che racchiudono le migliori ricette salutari").css("font-size", "18px");
+        $("#centered2").css("background-color", "rgba(255, 255, 255, 0.4)");
+      });
+      $("#centered2").mouseleave(function(){
+        $("#centered2").text(or2).css("font-size","28px");
+        $("#centered2").css("font-style", "normal");
+        $("#centered2").css("background-color", "transparent");
+      });
+
+      // third category 
+      var or3 = $("#centered3").text();
+      $("#centered3").mouseenter(function(){
+        $("#centered3").text("Quanto sono importanti l'allenamento e l'attività fisica nella nostra vita?").css("font-size", "18px");
+        $("#centered3").css("background-color", "rgba(255, 255, 255, 0.4)");
+      }, 1000);
+      $("#centered3").mouseleave(function(){
+        $("#centered3").text(or3).css("font-size","28px");
+        $("#centered3").css("font-style", "normal");
+        $("#centered3").css("background-color", "transparent");
+      });
+
+
+
+  });
