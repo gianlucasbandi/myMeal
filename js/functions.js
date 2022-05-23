82f77de8ac5fe7d481abb8722da40d883e23dc44
alessiapontiggia <102748217+alessiapontiggia@users.noreply.github.com>
2022-05-23 09:58:14 +0200

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -72,23 +72,39 @@ let swiper = new Swiper(".mySwiper", {
   $(document).ready(function(){
 
     // gallery fading images
-   
-      setInterval(function(){
-        $(".meal-img").fadeOut("slow", function(){
-          $(".meal-img").attr("src", "./img/second.jpg");
-          $(".meal-img").fadeIn("slow");
-          $(".meal-img").fadeOut("slow", function(){
-            $(".meal-img").attr("src", "./img/first.jpg");
-            $(".meal-img").fadeIn("slow");
+
+    var counter = 0;
+     t1 = setInterval(function(){
+        $(".meal-img").fadeOut(3000, function(){
+          $(".meal-img").attr("src", "./img/2.jpg");
+          $(".meal-img").fadeIn(3000);
+          $(".meal-img").fadeOut(3000, function(){
+            $(".meal-img").attr("src", "./img/1.jpg");
+           // $(".meal-img").fadeIn("slow");
           });
         });
-        /*
-        $(".meal-img").fadeOut("slow");
-        $(".meal-img").attr("src", "./img/first.jpg");
-        $(".meal-img").fadeIn("slow");
-        */
-      }, 3000);
-    
+        counter+=1;
+        if (counter==1) clearInterval(t1);
+        
+      }, 4000);
+      
+      var c=0;
+      t2 = setInterval(function(){
+        $(".meal-img").fadeOut(3000, function(){
+          $(".meal-img").attr("src", "./img/5.jpg");
+          $(".meal-img").fadeIn(3000);
+          $(".meal-img").fadeOut(3000, function(){
+            $(".meal-img").attr("src", "./img/4.jpg");
+            $(".meal-img").fadeIn(3000);
+          });
+        });
+        c+=1;
+        if (c==1){
+          clearInterval(t2);
+        }
+      }, 4000);
+      
+
    
     // first-category
     var original = $("#centered1").text();
