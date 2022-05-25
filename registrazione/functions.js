a474d4b38d6807810ace25e4327d94fa1277449a
Gianluca Sbandi <sbandi.gianluca@gmail.com>
2022-05-25 14:34:08 +0200

    valida email

--- registrazione/functions.js
+++ registrazione/functions.js
@@ -1,30 +1,40 @@
-document.querySelector('#registrationButton').addEventListener('click',valida);
+document.querySelector('#registrationButton').addEventListener('click', valida);
 let messaggi = document.querySelector('#messaggi');
 
 function valida() {
-    let data = new FormData();
-    data.append('inputNome',document.querySelector('#inputNome').value);
-    data.append('inputCognome',document.querySelector('#inputCognome').value);
-    data.append('inputEmail',document.querySelector('#inputEmail').value);
-    data.append('inputPasswd',document.querySelector('#inputPasswd').value);
-    data.append('registrationButton','accedi');
+    let email = document.querySelector('#inputEmail').value;
+    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+        messaggi.innerHTML = '<p style="color:red;">email non valida</p>';
+    }else{
+        let data = new FormData();
+    data.append('inputNome', document.querySelector('#inputNome').value);
+    data.append('inputCognome', document.querySelector('#inputCognome').value);
+    data.append('inputEmail', email);
+    data.append('inputPasswd', document.querySelector('#inputPasswd').value);
+    data.append('registrationButton', 'accedi');
     fetch('./validateRegistration.php', {
-      method: 'POST',
-      header: {
-        'Content-Type': 'application/json'
-      },
-      body: data
+        method: 'POST',
+        header: {
+            'Content-Type': 'application/json'
+        },
+        body: data
     })
-      .then(response => response.json())
-      .then(data => {
-        if (data['messaggio'] == "email già presente") {
-            messaggi.innerHTML = '<p style="color:red;">email già presente</p>';
-        }
-        else {
-            window.location.replace('../index.php');
-        }
-      })
-      .catch((error) => {
-        console.error('Errore: ', error);
-      });
-  }
+        .then(response => response.json())
+        .then(data => {
+            if (data['messaggio'] == "email già presente") {
+                messaggi.innerHTML = '<p style="color:red;">email già presente</p>';
+            }
+            else {
+                window.location.replace('../index.php');
+            }
+        })
+        .catch((error) => {
+            console.error('Errore: ', error);
+        });
+    }
+    
+}
+
+function ValidateEmail(mail) {
+    
+}
